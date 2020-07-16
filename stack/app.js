1a140071fe29cc1490e2d93cfe7362639bec4c93
sachin <knowsach@gmail.com>
2020-07-17 01:16:39 +0530

    stack implemented

---
+++ stack/app.js
@@ -0,0 +1,34 @@
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(element) {    //adds element in stack
+        this.stack.push(element);
+    }
+
+    pop() {            //removes element from stack
+        if (!this.stack.length) {
+            console.log('under flow.');
+            return;
+        }
+        this.stack.pop();
+    }
+
+    printStack() {       //print all elements in stack
+        this.stack.forEach(s => {
+            console.log('stackk elements:', s);
+        })
+    }
+}
+
+let stack = new Stack();
+stack.pop();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.printStack();
+
+stack.pop();
+stack.printStack();
